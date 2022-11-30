16974537494497ab9c272bfb4243252d8b20ce76
andreazetyawan <andreazetyawan@andreazetyawan.id>
2022-11-30 09:35:41 +0700

    update package routes

--- go.mod
+++ go.mod
@@ -1,3 +1,5 @@
 module github.com/andreazetyawan/simpel-backend-golang
 
 go 1.18
+
+require github.com/go-chi/chi/v5 v5.0.7 // indirect
--- main.go
+++ main.go
@@ -1,9 +1,35 @@
 package main
 
 import (
-	"fmt"
+	"net/http"
+	"os"
+	
+	"github.com/go-chi/chi/v5"
+	"github.com/go-chi/chi/v5/middleware"
+
+	"github.com/andreazetyawan/routes"
 )
 
+func getPort() string {
+	port := os.Getenv("PORT")
+	if port == "" {
+		port = ":3333"
+	} 
+	port = ":" + port
+
+	return port
+}
+
 func main(){
-	fmt.Println("Hello World");
+	f := routes.user()
+	// r := chi.NewRouter()
+	// r.Use(middleware.RequestID)
+	// r.Use(middleware.Logger)
+	// r.Use(middleware.Recoverer)
+
+	// r.Get("/", func(w http.ResponseWriter, r *http.Request) {
+	// 	w.Write([]byte("hello world"))
+	// })
+
+	http.ListenAndServe(getPort(), f)
 }
